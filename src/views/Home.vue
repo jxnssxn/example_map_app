6f156548b414269dcf0ad946b55a013f7d933674
jimmy <jonssonjimmy8@gmail.com>
2020-12-22 20:30:53 -0500

    set div for map height-300px, create env file for api key

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="home">
-    <div id="map">
+    <div id="map"></div>
     <h1>{{ message }}</h1>
-    </div>
   </div>
 </template>
 
 <style>
+body {
+  margin: 0;
+  padding: 0;
+}
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  height: 300px;
+  width: 500px;
+}
 </style>
 
 <script>
@@ -16,7 +26,16 @@ export default {
       message: "Welcome to Vue.js!",
     };
   },
-  created: function () {},
+  mounted: function () {
+    console.log(process.env.VUE_APP_MY_API_KEY);
+    mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+    var map = new mapboxgl.Map({
+      container: "map", // container id
+      style: "mapbox://styles/mapbox/streets-v11", // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  },
   methods: {},
 };
 </script>
